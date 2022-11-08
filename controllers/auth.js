0bb306b2fbd1566554cc0ceec2186dfffb32aa9c
danielmed <daniel.medina@talent.com>
2022-11-07 23:48:57 -0500

    register user model done

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,31 @@
-function register(req, res) {
-  console.log("registro ejecutado");
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
-  res.status(200).send({ msg: "All Good" });
+function register(req, res) {
+  const { firstname, lastname, email, password } = req.body;
+
+  if (!email) res.status(400).send({ msg: "El email es obligatorio" });
+  if (!password) res.status(400).send({ msg: "La contraseña es obligatoria" });
+
+  const user = new User({
+    firstname,
+    lastname,
+    email: email.toLowerCase(),
+    role: "user",
+    active: false,
+  });
+
+  const salt = bcrypt.genSaltSync(10);
+  const hashPassword = bcrypt.hashSync(password, salt);
+  user.password = hashPassword;
+
+  user.save((error, userStorage) => {
+    if (error) {
+      res.status(400).send({ msg: "Error creating user" });
+    } else {
+      res.status(200).send(userStorage);
+    }
+  });
 }
 
 module.exports = {
