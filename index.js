d669a7e6013f45b3b1930176b938e58af8c51e21
danielmed <daniel.medina@talent.com>
2022-11-07 22:49:26 -0500

    the initial configuration is done with all dependencies needded

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const app = require("./app");
 const {
   DB_USER,
   DB_PASSWORD,
@@ -7,10 +8,18 @@ const {
   API_VERSION,
 } = require("./constants");
 
+
+const PORT = process.env.POST || 3977;
+
 mongoose.connect(
   `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/`,
   (error) => {
     if(error) throw error;
-    console.log('the connection with the database was successful')
+    app.listen(PORT,()=>{
+      console.log("##################")
+      console.log("#### API REST ####")
+      console.log("##################")
+      console.log(`http://${IP_SERVER}:${PORT}/api/${API_VERSION}`)
+    })
   }
 );
