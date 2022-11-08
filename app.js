cafd53c6473d1bd12411ec7c3f6a57251a559d56
danielmed <daniel.medina@talent.com>
2022-11-08 15:14:44 -0500

    api for register user done

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bodyParser = require("body-parser");  
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const { API_VERSION } = require("./constants");
 
@@ -7,19 +7,20 @@ const app = express();
 
 // Import Rounting
 const authRoutes = require("./router/auth");
+const userRoutes = require("./router/user");
 
 // Configure Body Parse
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Configure static files
 app.use(express.static("uploads"));
 
 // Configure Header HTTP - CORS
-app.use(cors())
+app.use(cors());
 
 // Configure Routings
-app.use(`/api/${API_VERSION}`,authRoutes)
+app.use(`/api/${API_VERSION}`, authRoutes);
+app.use(`/api/${API_VERSION}`, userRoutes);
 
 module.exports = app;
- 
